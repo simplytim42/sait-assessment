248d840d2d580ac9a2853e3601465aaacfe4cce5
Tim Mackay <simplytim42@gmail.com>
2022-05-11 14:04:15 +0100

    second batch

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask, redirect, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+UPLOAD_FOLDER = '/tmp/'
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['GET'])
 def hello_world():
     return render_template("index.html")
 
-@app.route("/upload")
-def upload():
-    return render_template("upload.html")
+
+@app.route("/upload-file", methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    filename = file.filename
+    file.save(os.path.join(UPLOAD_FOLDER, filename))
+    return redirect(url_for('uploaded', name=filename))
+
+
+@app.route("/uploaded/<string:name>", methods=['GET'])
+def uploaded(name):
+    filepath = UPLOAD_FOLDER + name
+
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    return render_template("uploaded.html", lines=lines, filename=name)
+
 
 
 if __name__ == "__main__":
