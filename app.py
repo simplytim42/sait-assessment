3e0715fd78f1a4e0b628a5234555f2ad6b5c340e
Tim Mackay <simplytim42@gmail.com>
2022-05-11 15:24:10 +0100

    add analyse option

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 
 UPLOAD_FOLDER = '/tmp/'
-
 app = Flask(__name__)
 
 
@@ -16,17 +15,21 @@ def upload_file():
     file = request.files['file']
     filename = file.filename
     file.save(os.path.join(UPLOAD_FOLDER, filename))
-    return redirect(url_for('uploaded', name=filename))
+    return redirect(url_for('uploaded', filename=filename))
 
 
-@app.route("/uploaded/<string:name>", methods=['GET'])
-def uploaded(name):
-    filepath = UPLOAD_FOLDER + name
+@app.route("/uploaded/<string:filename>", methods=['GET'])
+def uploaded(filename):
+    filepath = UPLOAD_FOLDER + filename
 
     file = open(filepath, 'r')
     lines = file.readlines()
-    return render_template("uploaded.html", lines=lines, filename=name)
+    return render_template("uploaded.html", lines=lines, filename=filename)
 
+
+@app.route("/analyse/<string:filename>")
+def analyse(filename):
+    return render_template("analyse.html", filename=filename)
 
 
 if __name__ == "__main__":
